f0518541ae2621375379232e1af48c86975f7cbc
yunosuke <26004836+YunosukeYoshino@users.noreply.github.com>
2022-03-02 16:36:01 +0900

    条件分岐

--- practice-1/index.js
+++ practice-1/index.js
@@ -1,15 +1,25 @@
 'use strict';
-console.log('it\'s small world');
-console.log('hello\nworld');
-console.log('hello' + 'world');
+// console.log('it\'s small world');
+// console.log('hello\nworld');
+// console.log('hello' + 'world');
 
-let price = 500;
-price += 500;
+// let price = 500;
+// price += 500;
 
-price *= 2;
+// price *= 2;
 
-console.log(typeof null);
-console.log('8' + 3);
-console.log(parseInt('5', 10) + 3);
-console.log(Boolean(0));
-console.log(Boolean('hello'));
+// console.log(typeof null);
+// console.log('8' + 3);
+// console.log(parseInt('5', 10) + 3);
+// console.log(Boolean(0));
+// console.log(Boolean('hello'));
+
+const score = 90;
+
+if (score >= 80) {
+  console.log('Great!');
+} else if (score >= 60) {
+  console.log('Good.');
+} else {
+  console.log('OK...');
+}
